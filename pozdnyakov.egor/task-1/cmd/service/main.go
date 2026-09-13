2f0862c8157b790ce2f6a54a5911ace0f264e05e
R0me0Star <eip_spb@mail.ru>
2026-09-13 18:46:35 +0300

    [TASK-1] Invalid operations check added

--- pozdnyakov.egor/task-1/cmd/service/main.go
+++ pozdnyakov.egor/task-1/cmd/service/main.go
@@ -6,9 +6,23 @@ func main() {
 	var a, b int
 	var op string
 
-	fmt.Scan(&a)
-	fmt.Scan(&b)
-	fmt.Scan(&op)
+	_, err := fmt.Scan(&a)
+	if err != nil {
+		fmt.Println("First operand is invalid")
+		return
+	}
+
+	_, err = fmt.Scan(&b)
+	if err != nil {
+		fmt.Println("Second operand is invalid")
+		return
+	}
+
+	_, err = fmt.Scan(&op)
+	if err != nil {
+		fmt.Println("Operation is invalid")
+		return
+	}
 
 	switch op {
 	case "+":
@@ -18,6 +32,12 @@ func main() {
 	case "*":
 		fmt.Println(a * b)
 	case "/":
-		fmt.Println(a / b)
+		if b == 0 {
+			fmt.Println("Division by zero is prohibited")
+		} else {
+			fmt.Println(a / b)
+		}
+	default:
+		fmt.Println("Operation is invalid")
 	}
 }
